f5c3b3c1c8a1e6c43b7835affc0295083e019295
sunjangyo12 <sunjangyo12@gmail.com>
2025-06-14 19:17:09 +0700

    fix all cross build toolschains for termux

--- qemu_mode/build_android_support.sh
+++ qemu_mode/build_android_support.sh
@@ -1,5 +1,5 @@
 #############
-# by https://alephsecurity.com
+# by https://alephsecurity.com & sunjangyo12@gmail.com
 #############
 
 mkdir build
@@ -7,11 +7,11 @@ echo "=================="
 echo "Creating build folder."
 echo "=================="
 sleep 2
-export CC="/media/jin/4abb279b-6d65-4663-97c2-26987f64673a/home/yuna/LabTes/fuzzing-firmware/termux/android-ndk-r25c/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android30-clang"
-export CXX="/media/jin/4abb279b-6d65-4663-97c2-26987f64673a/home/yuna/LabTes/fuzzing-firmware/termux/android-ndk-r25c/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android30-clang++"
+export CC="/media/jin/4abb279b-6d65-4663-97c2-26987f64673a/home/yuna/LabTes/fuzzing-firmware/termux/android-ndk-r22b/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android30-clang"
+export CXX="/media/jin/4abb279b-6d65-4663-97c2-26987f64673a/home/yuna/LabTes/fuzzing-firmware/termux/android-ndk-r22b/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android30-clang++"
 export BUILD_DIR=`realpath ./build/`
 
-export TOOLCHAIN="/media/jin/4abb279b-6d65-4663-97c2-26987f64673a/home/yuna/LabTes/fuzzing-firmware/termux/android-ndk-r25c/toolchains/llvm/prebuilt/linux-x86_64"
+export TOOLCHAIN="/media/jin/4abb279b-6d65-4663-97c2-26987f64673a/home/yuna/LabTes/fuzzing-firmware/termux/android-ndk-r22b/toolchains/llvm/prebuilt/linux-x86_64"
 export QEMU_DIR="/media/jin/4abb279b-6d65-4663-97c2-26987f64673a/home/yuna/LabTes/fuzzing-firmware/termux/AFLplusplus/qemu_mode"
 
 
@@ -19,34 +19,37 @@ export QEMU_DIR="/media/jin/4abb279b-6d65-4663-97c2-26987f64673a/home/yuna/LabTe
 
 function build_iconv() {
 #wget https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.16.tar.gz
+rm -rf libiconv-1.16
 tar zxf libiconv-1.16.tar.gz
 cd libiconv-1.16
-./configure --prefix=$BUILD_DIR --host=aarch64-linux-android30  --disable-rpath
+./configure --prefix=$BUILD_DIR --host=aarch64-linux-android  --disable-rpath
 make -j 1
 make install
 cd ..
 echo;echo "=================="
 echo "build iconv done."
 echo "=================="
-read -p "Press Enter to continue"
+#read -p "Press Enter to continue"
 }
 
 function build_ffi() {
 #wget https://github.com/libffi/libffi/releases/download/v3.4.2/libffi-3.4.2.tar.gz
+rm -rf libffi-3.4.2/
 tar zxf libffi-3.4.2.tar.gz
 cd libffi-3.4.2/
-./configure --prefix=$BUILD_DIR --host=aarch64-linux-android30
+./configure --prefix=$BUILD_DIR --host=aarch64-linux-android
 make -j 1
 make install
 cd ..
 echo;echo "=================="
 echo "build libffi done."
 echo "=================="
-read -p "Press Enter to continue"
+#read -p "Press Enter to continue"
 }
 
 function build_gettext() {
 #wget https://ftp.gnu.org/gnu/gettext/gettext-0.19.tar.gz
+rm -rf gettext-0.19/
 tar zxf gettext-0.19.tar.gz
 cd gettext-0.19/
 ./configure --prefix=$BUILD_DIR --host=aarch64-linux-android30 --disable-threads
@@ -56,29 +59,45 @@ cd ..
 echo;echo "=================="
 echo "build gettext done."
 echo "=================="
-read -p "Press Enter to continue"
+#read -p "Press Enter to continue"
 }
 
 
 function build_glib() {
 #wget https://download.gnome.org/sources/glib/2.57/glib-2.57.1.tar.xz
+rm -rf glib-2.57.1
 tar xf glib-2.57.1.tar.xz
 cd glib-2.57.1; cp ../android.cache .
 chmod -w android.cache
-CFLAGS="-L$BUILD_DIR/lib -I$BUILD_DIR/include" ./configure --prefix=$BUILD_DIR --host=aarch64-linux-android30 --cache-file=android.cache --with-pcre=no  --enable-libmount=no --with-libiconv=gnu --disable-libelf
+CFLAGS="-L$BUILD_DIR/lib -I$BUILD_DIR/include" ./configure --prefix=$BUILD_DIR --host=aarch64-linux-android --cache-file=android.cache --with-pcre=no  --enable-libmount=no --with-libiconv=gnu --disable-libelf
 make -j 1
 make install
 cd ..
 echo;echo "=================="
 echo "build glibc done."
 echo "=================="
-read -p "Press Enter to continue"
+#read -p "Press Enter to continue"
+}
+
+function build_zlib() {
+#wget https://zlib.net/zlib-1.3.1.tar.gz
+#tar xf zlib-1.3.1.tar.gz
+cd zlib-1.3.1
+./configure --prefix=$BUILD_DIR
+make
+make install
+echo;echo "=================="
+echo "build zlib done."
+echo "=================="
+#read -p "Press Enter to continue"
 }
 
 #build_iconv
 #build_ffi
 #build_gettext
 #build_glib
+#build_zlib
 
-#(cd $QEMU_DIR ; sudo --preserve-env=CC,CXX PATH=$PATH:$TOOLCHAIN/bin NO_CHECKOUT=1 HOST=aarch64-linux-android30 PKG_CONFIG_PATH=$BUILD_DIR/lib/pkgconfig CPU_TARGET=aarch64 CROSS=$CC  ./build_qemu_support.sh)
-(cd $QEMU_DIR ; sudo --preserve-env=CC,CXX PATH=$PATH:$TOOLCHAIN/bin NO_CHECKOUT=1 PKG_CONFIG_PATH=$BUILD_DIR/lib/pkgconfig CPU_TARGET=aarch64 CROSS=$CC  ./build_qemu_support.sh)
+
+(cd $QEMU_DIR;sudo --preserve-env=CC,CXX PATH=$PATH:$TOOLCHAIN/bin NO_CHECKOUT=1 HOST="aarch64-linux-android" PKG_CONFIG=pkg-config PKG_CONFIG_PATH=$BUILD_DIR/lib/pkgconfig CPU_TARGET=aarch64 CROSS=$CC  ./build_qemu_support.sh)
+
